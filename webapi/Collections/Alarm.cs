ea54d641c9acbe8066507c8950817d3634878ad3
Sandro Räss <s.raess@me.com>
2026-07-03 23:07:08 +0200

    Update Alarm.cs to use BsonId.

--- webapi/Collections/Alarm.cs
+++ webapi/Collections/Alarm.cs
@@ -1,7 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace webapi.Collections;
 
 public class Alarm
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
     public string HorseId { get; set; } = string.Empty;
 
     public AlarmType Type { get; set; }
